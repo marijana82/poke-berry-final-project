e4e71658e11e6a7ecd4f0c5fe8e213d248341303
marijana82 <marijana.mikolcic@gmail.com>
2023-09-06 22:02:03 +0200

    created simple design for landing page

--- src/pages/landing-page/Landing.js
+++ src/pages/landing-page/Landing.js
@@ -1,10 +1,40 @@
 import "./Landing.css";
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+//import header
+//import footer
+//import main
+//do some other imports, check the old project to see what's missing
+
 
 
 function Landing() {
+    const navigate = useNavigate();
+
+
+
     return (
         <>
+            <div className="outer-content-box-landing-page">
+                <caption>
+                    <h1>A simple application that helps you keep your pokemon in one place and let them eat those berries.</h1>
+                </caption>
+
+                <div className="inner-content-box-landing-page">
+                    <article>
+                        <h2>To get a glimpse of what PokeBerry has to offer, click on the card...</h2>
+                        <img alt="image of an arrow pointing to the flippable card"/>
+                        <p>...or click <Link to={"/registration-page"}>here</Link> to register now!</p>
+                    </article>
+                </div>
+                <div className="content-box-flippable-card">
+
+                    HERE COMES COMPONENT FOR THE FLIPPABLE CARD
+                </div>
+
+            </div>
+
+
 
         </>
     )
