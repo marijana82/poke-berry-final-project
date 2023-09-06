f11767ae7299a694e1e048369932599b4e8dd301
marijana82 <marijana.mikolcic@gmail.com>
2023-09-06 22:04:29 +0200

    imported landing page to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React, { useContext } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 //import { LoginContext } from "./context/LoginContext";
+import Landing from "./pages/landing-page/Landing";
 
 function App() {
   return (
     <>
       HELLO POKE BERRIES!
-        NEXT STEP IS TO MAKE HTML AND CSS FOR ALL PAGES, BUT THAT I WILL DO LATER!
+        NEXT STEP IS TO finish working on the landing page, tomorrow! BYE!
     </>
   );
 }
