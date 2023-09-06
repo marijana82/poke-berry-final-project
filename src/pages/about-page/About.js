b78e5b5c83c06f28e70abf6da37698939668c354
marijana82 <marijana.mikolcic@gmail.com>
2023-09-06 18:45:18 +0200

    about-page structure composed

--- src/pages/about-page/About.js
+++ src/pages/about-page/About.js
@@ -5,6 +5,33 @@ import React from "react";
 function About() {
     return (
         <>
+            <div className="introduction-container">
+                <h1>About the Poke Berry App</h1>
+            </div>
+
+            <section>
+                <article>
+                    <h2>Why PokeBerry?</h2>
+                    <p>bla bla bla</p>
+                    <p>bla bla bla</p>
+                </article>
+            </section>
+
+            <section>
+                <article>
+                    <h2>Why not PokeBerry?</h2>
+                    <p>bla bla bla</p>
+                    <p>bla bla bla</p>
+                </article>
+            </section>
+
+            <section>
+                <article>
+                    <h2>How to get in touch?</h2>
+                    <p>bla bla bla</p>
+                    <p>bla bla bla</p>
+                </article>
+            </section>
 
         </>
     )
