1068315b5de4666c1e89f0989bc614aba17414de
marijana82 <marijana.mikolcic@gmail.com>
2023-09-06 21:53:40 +0200

    css for navbar landing page created

--- src/components/navbar-landing-page/NavBarLandingPage.js
+++ src/components/navbar-landing-page/NavBarLandingPage.js
@@ -2,24 +2,31 @@ import "./NavBarLandingPage.css";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+//to this navbar I can further integrate a <SearchBar/> , I just have to put a component between a <li></li>
+//in css listnavbar is equivalent to my ul
+
 function NavBarLandingPage() {
     return(
         <nav className="outer-container-navigation">
             <div className="inner-container-navigation">
-                <ul className="navigation-content">
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/registration-page"
+                            className={({isActive}) => isActive === true ? "active-link" : "default-link"}
+                        >Register
+                        </NavLink>
+                    </li>
 
-                    <NavLink
-                        to="/registration-page"
-                        className={({isActive}) => isActive === true ? "active-link" : "default-link"}
-                    >Register
-                    </NavLink>
+                    <li>
+                        <NavLink
+                            to="/login-page"
+                            className={({isActive}) => isActive === true ? "active-link" : "default-link"}
+                        >Login
+                        </NavLink>
+                    </li>
 
-                    <NavLink
-                        to="/login-page"
-                        className={({isActive}) => isActive === true ? "active-link" : "default-link"}
-                    >Login
-                    </NavLink>
-
+                    <li>HERE CAN COME A SMALL SEARCH BAR</li>
 
                 </ul>
             </div>
