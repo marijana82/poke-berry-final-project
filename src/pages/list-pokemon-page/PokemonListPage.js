7513491447dde7757446e8f7fdc7f300cf622369
marijana82 <marijana.mikolcic@gmail.com>
2023-09-07 19:16:55 +0200

    started working on pokemon list

---
+++ src/pages/list-pokemon-page/PokemonListPage.js
@@ -0,0 +1,24 @@
+import "./PokemonListPage.css";
+import React from "react";
+
+
+function PokemonListPage() {
+
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">
+                    HERE COMES CARD
+
+
+                </div>
+                <div className="right-content">
+                    HERE COMES POKE INFO
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PokemonListPage;
