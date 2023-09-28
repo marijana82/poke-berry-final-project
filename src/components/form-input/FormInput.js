a43b9c45617988c09af69943781053c5ced72fd2
marijana82 <marijana.mikolcic@gmail.com>
2023-09-28 13:41:47 +0200

    FormInput component created

---
+++ src/components/form-input/FormInput.js
@@ -0,0 +1,24 @@
+import "./FormInput.css";
+import React from "react";
+
+
+function FormInput({ idAttribute, labelText, inputType, placeholder, nameAttribute, stateValue, stateSetter}) {
+    return(
+        <div className="input-label-container">
+            <label
+                htmlFor={idAttribute}
+            >{labelText}
+                <input
+                    type={inputType}
+                    placeholder={placeholder}
+                    name={nameAttribute}
+                    id={idAttribute}
+                    value={stateValue}
+                    onChange={(e) => stateSetter(e.target.value)}
+                />
+            </label>
+        </div>
+    )
+}
+
+export default FormInput;
