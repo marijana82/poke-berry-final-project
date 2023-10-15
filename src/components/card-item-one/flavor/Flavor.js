4963f7d36f85d59d7cb862832ff4c1659d13c9b2
marijana82 <marijana.mikolcic@gmail.com>
2023-10-15 20:55:06 +0200

    created berryFilter variable/function with potency parameter, this function filters berries according to their potency and shows the filtered berries in a new list

--- src/components/card-item-one/flavor/Flavor.js
+++ src/components/card-item-one/flavor/Flavor.js
@@ -7,13 +7,20 @@ import axios from "axios";
 function Flavor({flavorUrl}) {
 
     const [flavorData, setFlavorData] = useState(null);
+    const [filteredBerries, setFilteredBerries] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
     async function fetchFlavorData() {
         try {
-
             const resultFlavor = await axios.get(flavorUrl);
             console.log(resultFlavor.data);
             setFlavorData(resultFlavor.data);
+
+            if(resultFlavor.data.berries) {
+                setFilteredBerries(resultFlavor.data.berries);
+            } else {
+                setErrorMessage("Cannot filter berries");
+            }
 
         } catch(e) {
             console.error(e);
@@ -22,7 +29,16 @@ function Flavor({flavorUrl}) {
 
     useEffect(() => {
         fetchFlavorData();
-    }, []);
+    }, [flavorUrl]);
+
+
+    //filter function/variable
+    const berryFilter = (potency) => {
+        let filteredPotency = flavorData.berries.filter(berry => berry.potency === potency);
+        setFilteredBerries(filteredPotency);
+    }
+
+
 
     return(
         <>
@@ -30,19 +46,49 @@ function Flavor({flavorUrl}) {
                 {flavorData &&
                     <div>
                         <h3>{flavorData.name} berry is {flavorData.contest_type.name}!</h3>
+                        <div className="button-group-container">
+                            <button onClick={() => berryFilter(10)}>10</button>
+                            <button onClick={() => berryFilter(15)}>15</button>
+                            <button onClick={() => berryFilter(20)}>20</button>
+                            <button onClick={() => berryFilter(25)}>25</button>
+                            <button onClick={() => berryFilter(30)}>30</button>
+                        </div>
+
+
                         <h3>All {flavorData.name} berries in one list: </h3>
+
 
                         {
                             flavorData &&
                             flavorData.berries &&
-                            flavorData.berries.map((berry) => {
+                            filteredBerries
+
+                            ?
+
+                            filteredBerries.map((pokemon, index) => (
+                                <li key={index}>
+                                    <p>{pokemon.berry.name}</p>
+                                    <p>{pokemon.potency}</p>
+                                </li>
+                            ))
+
+                            :
+
+                            <p>{errorMessage}</p>
+
+
+                            /*flavorData.berries.map((berry, index) => {
                                 return(
-                                    <li>
-                                        <p>{berry.berry.name}</p>
-                                        <p>{berry.potency}</p>
-                                    </li>
+                                    <>
+                                        <li key={index}>
+                                            <p>{berry.berry.name}</p>
+                                            <p>{berry.potency}</p>
+                                        </li>
+
+                                    </>
+
                                 )
-                            })
+                            })*/
                         }
 
                     </div>
